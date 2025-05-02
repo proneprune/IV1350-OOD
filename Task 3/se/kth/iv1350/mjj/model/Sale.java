4a3a2fb9eec5928b7499f75374130d8a258ce27a
Jonathan Nilsson Cullgert <jonnc@kth.se>
2025-05-02 16:31:01 +0200

    Javadoc comments for Sale.java

--- Task 3/se/kth/iv1350/mjj/model/Sale.java
+++ Task 3/se/kth/iv1350/mjj/model/Sale.java
@@ -16,12 +16,20 @@ public class Sale {
     private ArrayList<Entry<ProductDTO, Integer>> productList;
 
 
-
+    /**
+     *  Creates a Sale object with an empty list of products and a price object.
+     */ 
     public Sale() {
         this.price = new Price();
         this.productList = new ArrayList<Entry<ProductDTO, Integer>>();
     }
 
+    /**
+     * Adds a product to the sale. If the product already exists in the list, it updates the quantity.
+     * 
+     * @param product The product to be added to the sale.
+     * @param quantity The amount of the product to be added.
+     */
     public void addProduct(ProductDTO product, int quantity) {
         boolean productExists = false;
         for (Entry<ProductDTO, Integer> entry : this.productList) {
@@ -39,15 +47,33 @@ public class Sale {
     }
 
 
+    /**
+     * Gets the total price of the Sale.
+     * Since VAT is included in the price in the inventory, get total price 
+     * is the total price including VAT.
+     * 
+     * @return The total price of the sale including VAT.
+     */
     public double getRunningTotalPlusVat() {
         return this.price.getTotalPrice();
     }
 
+    /**
+     * Creates a SaleDTO object from the current sale.
+     * 
+     * @return A SaleDTO object containing the total price, total tax price, and list of products in the sale.
+     */
     public SaleDTO getSaleDTO() {
         return new SaleDTO(this.price.getTotalPrice(), this.price.getTotalTaxPrice(), this.productList);
         //return this.productList;
     }
 
+    /**
+     * Updates the discount for the sale.
+     * This method is not implemented in sem3.
+     * 
+     * @param discount The discount to be applied to the sale.
+     */
     public void updateDiscount(double discount) {
         //this discount method is not implemented in sem3
     }
