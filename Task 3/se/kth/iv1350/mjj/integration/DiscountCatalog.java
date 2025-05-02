f690c336abe2aee8d9c0fd3f41169c7a53704102
Joel Friis <joefri@kth.se>
2025-05-02 16:31:48 +0200

    added display

---
+++ Task 3/se/kth/iv1350/mjj/integration/DiscountCatalog.java
@@ -0,0 +1,14 @@
+package se.kth.iv1350.mjj.integration;
+
+import se.kth.iv1350.mjj.model.DTO.SaleDTO;
+
+public class DiscountCatalog {
+
+    public DiscountCatalog() {
+
+    }
+
+    public double getDiscount(SaleDTO saleToDiscount, int costumerId) {
+        return 0;
+    }
+}
