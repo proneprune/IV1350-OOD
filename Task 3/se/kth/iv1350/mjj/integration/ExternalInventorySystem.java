1af59d9bde79a5d5f03a5d32d5e660d549d39ca2
Malte Berg <malte@malte.nu>
2025-05-02 21:58:18 +0200

    Fix typo

--- Task 3/se/kth/iv1350/mjj/integration/ExternalInventorySystem.java
+++ Task 3/se/kth/iv1350/mjj/integration/ExternalInventorySystem.java
@@ -8,7 +8,7 @@ public class ExternalInventorySystem {
     private ProductDTO item1 = new ProductDTO(1, "Arboga", "Arboga 10.2%", 23.6, 0.25);
     private ProductDTO item2 = new ProductDTO(2, "Mjölk 1L", "Arla mellan mjölk 1L, 1.5% fett", 14.95, 0.12);
     private ProductDTO item3 = new ProductDTO(3, "Gräddfil Chips", "Estrella Gräddfil Chips, fett goda", 29.95, 0.12);
-    private ProductDTO item4 = new ProductDTO(3, "Toalettpapper 12 pack", "Lambi toalettpaper 12 pack extra mjukt", 89.90, 0.12);
+    private ProductDTO item4 = new ProductDTO(4, "Toalettpapper 12 pack", "Lambi toalettpaper 12 pack extra mjukt", 89.90, 0.12);
 
 
     public ExternalInventorySystem() {
