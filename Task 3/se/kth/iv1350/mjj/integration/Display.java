b97dcfe4f2c320ff29d6675c99f294dfc90ca7a2
Jonathan Nilsson Cullgert <jonnc@kth.se>
2025-05-02 20:49:08 +0200

    changed display to take in productDTO instead

--- Task 3/se/kth/iv1350/mjj/integration/Display.java
+++ Task 3/se/kth/iv1350/mjj/integration/Display.java
@@ -6,23 +6,41 @@ import se.kth.iv1350.mjj.model.DTO.ProductDTO;
 import se.kth.iv1350.mjj.model.DTO.SaleDTO;
 
 public class Display {
+    private double runningTotal;
+    private double totalTax;
 
-
+    /**
+     * Creates a new instance of Display.
+     */
     public Display() {
-
     }
 
-    public void updateDisplay(SaleDTO currentSale) {
-        ArrayList <ProductDTO> productsInSale = currentSale.getProductsInSale();
-        ProductDTO addedProduct = productsInSale.get(productsInSale.size()-1);
-        System.out.printf("Added 1 Item with ID: %d%n", addedProduct.getProductID());
-        System.out.printf("Item ID: %d%n", addedProduct.getProductID());
-        System.out.printf("Product name: %s%n", addedProduct.getProductName());
-        System.out.printf("Item cost: %f%n", addedProduct.getPrice());
-        System.out.printf("Item VAT: %f%n", addedProduct.getTaxRate());
-        System.out.printf("Item description: %s%n", addedProduct.getProductDesc());
-        System.out.printf("Total cost (incl VAT): %f%n", currentSale.getRunningTotal());
-        System.out.printf("Total VAT: %f%n", currentSale.getTotalTax());
+    /**
+     * Updates the display with the current product and quantity.
+     * aswell as all the relevant information about the product.
+     * 
+     * it also calculates running total and total tax for this instance of the sale.
+     * 
+     * @param currentProduct The current product to be displayed.
+     * @param quantity       The quantity of the current product.
+     */
+    public void updateDisplay(ProductDTO currentProduct, int quantity) {
+        runningTotal += currentProduct.getPrice() * quantity;
+        getTaxRate(currentProduct, quantity);
+
+        System.out.printf("Added %d Item with ID: %d%n", quantity, currentProduct.getProductID());
+        System.out.printf("Item ID: %d%n", currentProduct.getProductID());
+        System.out.printf("Product name: %s%n", currentProduct.getProductName());
+        System.out.printf("Item cost: %f%n", currentProduct.getPrice());
+        System.out.printf("Item VAT: %f%n", currentProduct.getTaxRate());
+        System.out.printf("Item description: %s%n", currentProduct.getProductDesc());
+
+        System.out.printf("Total cost (incl VAT): %f%n", runningTotal);
+        System.out.printf("Total VAT: %f%n", totalTax);
+    }
+
+    private void getTaxRate(ProductDTO currentProduct, int quantity) {
+        totalTax += (currentProduct.getPrice() - (currentProduct.getPrice()/(currentProduct.getTaxRate()+1))) * quantity;
     }
     
 }
