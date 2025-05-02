eb3a6981cefd2e7c67d06303cf6bc976c601fd27
Malte Berg <malte@malte.nu>
2025-05-02 23:11:35 +0200

    Testing has started 🦅🔥

---
+++ Task 3/se/kth/iv1350/mjj/tests/integration/ExternalInventorySystemTest.java
@@ -0,0 +1,31 @@
+package se.kth.iv1350.mjj.tests.integration;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import se.kth.iv1350.mjj.controller.SaleController;
+import se.kth.iv1350.mjj.integration.Display;
+import se.kth.iv1350.mjj.integration.ExternalAccountingSystem;
+import se.kth.iv1350.mjj.integration.ReceiptPrinter;
+
+public class ExternalInventorySystemTest {
+
+    private SaleController saleController;
+    
+    @BeforeEach
+    public void setUp() {
+        
+    }
+
+    @AfterEach
+    public void tearDown() {
+        
+    }
+
+    @Test
+    public void testScanProductCorrectID() {
+        
+    }
+}
