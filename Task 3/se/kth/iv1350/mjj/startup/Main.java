78f3064c20e2bbfb2a98a394d62779047e1b882f
Malte Berg <malte@malte.nu>
2025-05-02 21:23:29 +0200

    Added Main class functionality.

--- Task 3/se/kth/iv1350/mjj/startup/Main.java
+++ Task 3/se/kth/iv1350/mjj/startup/Main.java
@@ -2,10 +2,18 @@ package se.kth.iv1350.mjj.startup;
 
 import se.kth.iv1350.mjj.integration.ReceiptPrinter;
 import se.kth.iv1350.mjj.integration.Display;
+import se.kth.iv1350.mjj.integration.ExternalAccountingSystem;
+import se.kth.iv1350.mjj.integration.ExternalInventorySystem;
+import se.kth.iv1350.mjj.controller.SaleController;
+import se.kth.iv1350.mjj.view.View;
 
 public class Main {
     public static void main(String[] args) {
         ReceiptPrinter receiptPrinter = new ReceiptPrinter();
         Display display = new Display();
+        ExternalAccountingSystem externalAccountingSystem = new ExternalAccountingSystem();
+        ExternalInventorySystem externalInventorySystem = new ExternalInventorySystem();
+        SaleController saleController = new SaleController(externalAccountingSystem, externalInventorySystem, receiptPrinter, display);
+        View view = new View(saleController);
     }
 }
