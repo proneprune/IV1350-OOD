ebb7cd9bc4c7465585ae8bcc12ef2f368fdb37a2
Malte Berg <malte@malte.nu>
2025-05-02 21:36:58 +0200

    Added JavaDoc

--- Task 3/se/kth/iv1350/mjj/startup/Main.java
+++ Task 3/se/kth/iv1350/mjj/startup/Main.java
@@ -8,6 +8,13 @@ import se.kth.iv1350.mjj.controller.SaleController;
 import se.kth.iv1350.mjj.view.View;
 
 public class Main {
+
+    /**
+     * The method responsible for the startup of the program.
+     * It creates all systems in order according to the diagram.
+     * 
+     * @param args Additional arguments (Not in use)
+     */
     public static void main(String[] args) {
         ReceiptPrinter receiptPrinter = new ReceiptPrinter();
         Display display = new Display();
