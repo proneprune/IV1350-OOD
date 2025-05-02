6035d8049df837ae7e2d9340ad16be54ed260fd0
Malte Berg <malte@malte.nu>
2025-05-02 21:10:25 +0200

    Start of main class, startup.

--- Task 3/se/kth/iv1350/mjj/startup/Main.java
+++ Task 3/se/kth/iv1350/mjj/startup/Main.java
@@ -1,5 +1,11 @@
 package se.kth.iv1350.mjj.startup;
 
+import se.kth.iv1350.mjj.integration.ReceiptPrinter;
+import se.kth.iv1350.mjj.integration.Display;
+
 public class Main {
-    
+    public static void main(String[] args) {
+        ReceiptPrinter receiptPrinter = new ReceiptPrinter();
+        Display display = new Display();
+    }
 }
