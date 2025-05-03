00a864a761b281a30c10078fb2d303f1c9640c16
Malte Berg <malte@malte.nu>
2025-05-03 16:00:15 +0200

    Added and updated SaleController tests

--- Task 3/se/kth/iv1350/mjj/tests/controller/SaleControllerTest.java
+++ Task 3/se/kth/iv1350/mjj/tests/controller/SaleControllerTest.java
@@ -11,14 +11,17 @@ import se.kth.iv1350.mjj.integration.Display;
 import se.kth.iv1350.mjj.integration.ExternalAccountingSystem;
 import se.kth.iv1350.mjj.integration.ExternalInventorySystem;
 import se.kth.iv1350.mjj.integration.ReceiptPrinter;
+import se.kth.iv1350.mjj.model.DTO.ProductDTO;
 
 public class SaleControllerTest {
 
     private SaleController saleController;
+    private ExternalInventorySystem externalInventorySystem;
     
     @BeforeEach
     public void setUp() {
-        saleController = new SaleController(new ExternalAccountingSystem(), new ExternalInventorySystem(), new ReceiptPrinter(), new Display());
+        externalInventorySystem = new ExternalInventorySystem();
+        saleController = new SaleController(new ExternalAccountingSystem(), externalInventorySystem, new ReceiptPrinter(), new Display());
         saleController.startSale();
     }
 
@@ -29,18 +32,37 @@ public class SaleControllerTest {
 
     @Test
     public void testScanProductCorrectID() {
-        saleController.scanProduct(1, 1);
+        ProductDTO expected = new ProductDTO(2, "Mjölk 1L", "Arla mellan mjölk 1L, 1.5% fett", 14.95, 0.12);
+        ProductDTO tested = externalInventorySystem.getProductInfo(2);
+        assertEquals(expected.getProductID(), tested.getProductID());
+        assertEquals(expected.getProductName(), tested.getProductName());
+        assertEquals(expected.getProductDesc(), tested.getProductDesc());
+        assertEquals(expected.getPrice(), tested.getPrice());
+        assertEquals(expected.getTaxRate(), tested.getTaxRate());
     }
+
     @Test
     public void testDoAPurchase() {
         saleController.scanProduct(1, 1);
         double amountToPay = saleController.endSale();
         double change = saleController.enterAmount(amountToPay);
     }
+
     @Test
-    public void testPayingWithNegativMoney() {
+    public void testPayingWithNegativeMoney() {
         saleController.scanProduct(1, 1);
         double amountToPay = saleController.endSale();
-        double change = saleController.enterAmount(-amountToPay);
+        amountToPay *= -1;
+        double change = saleController.enterAmount(amountToPay);
+        assertEquals(change, -1, "Change is not -1 (error).");
+    }
+
+    @Test
+    public void testPayingInsufficentFunds() {
+        saleController.scanProduct(1, 1);
+        double amountToPay = saleController.endSale();
+        amountToPay--;
+        double change = saleController.enterAmount(amountToPay);
+        assertEquals(change, -2, "Change is not -2 (error).");
     }
 }
