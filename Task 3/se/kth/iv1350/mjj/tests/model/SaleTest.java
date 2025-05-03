8b1fab12318c602176e5b3d4be4a3f256414f986
Jonathan Nilsson Cullgert <jonnc@kth.se>
2025-05-03 14:46:34 +0200

    did a sale test to see that tax and runningtotal is correct

--- Task 3/se/kth/iv1350/mjj/tests/model/SaleTest.java
+++ Task 3/se/kth/iv1350/mjj/tests/model/SaleTest.java
@@ -5,27 +5,36 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import se.kth.iv1350.mjj.controller.SaleController;
-import se.kth.iv1350.mjj.integration.Display;
-import se.kth.iv1350.mjj.integration.ExternalAccountingSystem;
-import se.kth.iv1350.mjj.integration.ReceiptPrinter;
+
+import se.kth.iv1350.mjj.model.Sale;
+import se.kth.iv1350.mjj.model.DTO.ProductDTO;
 
 public class SaleTest {
 
-    private SaleController saleController;
+    private Sale sale;
+    private ProductDTO product;
     
     @BeforeEach
     public void setUp() {
-        
+        sale = new Sale();
+        product = new ProductDTO(0, "Milk", "Tasty milk", 10.0, 0.1);
     }
 
     @AfterEach
     public void tearDown() {
-        
+        sale = null;
+        product = null;
     }
 
     @Test
-    public void testScanProductCorrectID() {
-        
+    public void testAddNewProduct() {
+        sale.addProduct(product, 2);
+        double expectedTotal = 2 * 10.0;
+        double expectedTax = 2 * (10 - (10.0 / 1.1)) ;
+
+        assertEquals(expectedTotal, sale.getRunningTotalPlusVat(), 0.0001, "Total cost with VAT should be correct");
+        assertEquals(expectedTax, sale.getTotalTax(), 0.0001, "Tax cost should be correct");
     }
+
+
 }
