fff820a1390d9397d12a64b0569351d0c5797082
Jonathan Nilsson Cullgert <jonnc@kth.se>
2025-05-13 13:56:57 +0200

    test?

--- Task 4/se/kth/iv1350/mjj/util/DatabaseUnreachableException.java
+++
@@ -1,7 +0,0 @@
-package se.kth.iv1350.mjj.util;
-
-public class DatabaseUnreachableException extends Exception {
-    public DatabaseUnreachableException(String message) {
-        super(message);
-    }
-}
--- Task 4/se/kth/iv1350/mjj/util/ItemNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package se.kth.iv1350.mjj.util;
-
-public class ItemNotFoundException extends Exception {
-    public ItemNotFoundException(int productID) {
-        super("Item with ID " + productID + " not found in InventorySystem.");
-    }
-}
